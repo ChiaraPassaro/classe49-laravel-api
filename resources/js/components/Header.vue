a33c441579ab1f79b232a6a0800eb93ad07a8832
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-03-10 12:26:30 +0100

    header logo link

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,9 +1,8 @@
 <template>
    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
-          <a class="navbar-brand" href="#">
-              <img id="logo" class="w-25" :src="logo" alt="logo">
-          </a>
+          <router-link class="nav-link" :to="{ name: 'home'}"> <img id="logo" class="w-25" :src="logo" alt="logo"></router-link>
+             
           <button class="navbar-toggler" type="button" data-toggle="collapse"
                   data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                   aria-expanded="false">
